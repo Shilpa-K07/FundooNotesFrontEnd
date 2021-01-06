a5a8c96ef53ab124c4ef3484bb9f63c02900b2fc
Shilpa-K07 <shilpa07udupi@gmail.com>
2021-01-06 17:24:41 +0530

    refactor: registration page refactor in-progress

--- fundoo-notes-front-end/src/components/Registration.vue
+++ fundoo-notes-front-end/src/components/Registration.vue
@@ -1,29 +1,40 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <!--  <v-app-bar app>
       <v-toolbar-title>fundooNotes</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text>Sign Up</v-btn>
       <v-btn text>Login</v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
 
     <v-content>
-      <v-card width = "300" class='mx-auto mt-9'>
-        <v-card-title>Sign Up</v-card-title>
+      <v-card width="550" class="mx-auto mt-9 pt-7 pl-7 pr-7 pb-7">
+        <v-card-title>
+          fundooNotes
+          <br />Create your fundooNotes Account
+        </v-card-title>
         <v-card-text>
-          <v-text-field label='First Name' v-model='fName'/>
-          <v-text-field label='Last Name' v-model='lName'/>
-          <v-text-field label='Email Address' v-model='emailId' prepend-icon="mdi-account-circle"/>
-          <v-text-field label='Password' 
-          :type= "showPassword ? 'text': 'password'" v-model='password' prepend-icon="mdi-lock" 
-          :append-icon="showPassword ?'mdi-eye':'mdi-eye-off'"
-          @click:append="showPassword =!showPassword"/>
+          <div class="text-filed-align">
+            <v-text-field
+              d-flex
+              outlined
+              dense
+              label="First name"
+              v-model="post.fName"
+              class="sideBySide"
+            />
+            <v-text-field outlined dense label="Last name" v-model="post.lName" class="sideBySide" />
+          </div>
+          <v-text-field outlined dense label="Email address" v-model="post.emailId" />
+          <div>
+            <v-text-field outlined dense label="Password" v-model="post.password" />
+            <v-text-field outlined dense label="Password" v-model="post.password" />
+          </div>
         </v-card-text>
 
         <v-divider></v-divider>
         <v-card-actions>
-          <v-btn color='info'
-         @click='registration'>Register</v-btn>
+          <v-btn color="info" @click="registration">Register</v-btn>
         </v-card-actions>
       </v-card>
     </v-content>
@@ -31,20 +42,20 @@
 </template>
 <script>
 export default {
-  data()
-  {
-    return{
-      fName: null,
+  data() {
+    return {
+      /* fName: null,
       lName: null,
       emailId: null,
-      password: null,
-      post:{},
+      password: null, */
+      post: {},
       showPassword: false,
-      URL:'http://localhost:3000/registration'
-    }
+      URL: "http://localhost:3000/registration"
+    };
   },
   methods: {
-        registration() {
+    registration() {
+      console.log("data:" + JSON.stringify(this.post));
       /* const request = new Request(
         URL,
         {
@@ -58,24 +69,36 @@ export default {
       alert("data: "+data._id)
       
     } */
-    fetch(this.URL, {
-            method: 'POST',
-            mode: 'no-cors',
-            /* headers:{
+      fetch(this.URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        /* headers:{
               'Access-Control-Allow-Origin':'*',
               'Access-Control-Allow-Headers':'*'
             }, */
-            body: JSON.stringify({
+        body: JSON.stringify(
+          /* {
                 firstName : this.fName,
                 lastName : this.lName,
                 emailId: this.emailId,
                 password: this.password
-            })
-        })
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
-      }
+            } */ this
+            .post
+        )
+      })
+        .then(res => res.json())
+        .then(data => console.log("data: " + data))
+        .catch(err => console.log(err));
+    }
   }
-}
+};
 </script>
+<style scoped>
+.text-filed-align {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
