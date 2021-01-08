b1a19a924936827e8c92732e30fae266d6c359ad
Shilpa-K07 <shilpa07udupi@gmail.com>
2021-01-08 17:40:44 +0530

    wip: implement vuelidate for registration page in-progress

--- fundoo-notes-front-end/src/components/Registration.vue
+++ fundoo-notes-front-end/src/components/Registration.vue
@@ -1,4 +1,5 @@
 <template>
+<form>
   <v-app>
     <v-content>
       <v-card class="mx-auto mt-9 pl-9 pt-9" max-width="55%" height="80%" outlined>
@@ -26,22 +27,22 @@
               <v-row>
                 <v-col>
                   <v-text-field
-                    d-flex
                     outlined
                     dense
                     label="First name"
-                    v-model="user.fName"
-                    :rules="fNameRules"
+                    v-model="firstName"
+                    :error-messages="firstNameErrors"
                     required
                   />
+                 <!--  <p class="error" v-if="!$v.user.fName.required">this field is required</p> -->
                 </v-col>
                 <v-col>
                   <v-text-field
                     outlined
                     dense
                     label="Last name"
-                    v-model="user.lName"
-                    :rules="lNameRules"
+                    v-model="lastName"
+                    :error-messages="lastNameErrors"
                     required
                   />
                 </v-col>
@@ -53,8 +54,8 @@
                 outlined
                 dense
                 label="Email address"
-                v-model="user.emailId"
-                :rules="emailRules"
+                v-model="emailId"
+                :error-messages="emailIdErrors"
                 required
               />
               <v-row>
@@ -65,12 +66,11 @@
                   <v-text-field
                     outlined
                     dense
-                    persistent-hint
                     label="Password"
                     :type="showPassword ? 'text' : 'password'"
                     v-on:click="isClicked = true"
-                    v-model="user.password"
-                    :rules="passwordRules"
+                    v-model="password"
+                    :error-messages="passwordErrors"
                     required
                   />
                 </v-col>
@@ -80,7 +80,7 @@
                     dense
                     :type="showPassword ? 'text' : 'password'"
                     label="Confirm"
-                    v-model="user.confirm"
+                    v-model="confirmPassword"
                     required
                   />
                 </v-col>
@@ -100,7 +100,7 @@
                 <v-btn class="sign-in" text small color="info">Sign in instead</v-btn>
                 </div>
                 <v-col></v-col>
-                <v-btn color="info" @click="validate">Register</v-btn>
+                <v-btn color="info" @click="Register">Register</v-btn>
               </v-row>
             </v-card-text>
           </v-flex>
@@ -121,44 +121,77 @@
       </v-card>
     </v-content>
   </v-app>
+</form>
 </template>
 <script>
+import {required, minLength} from 'vuelidate/lib/validators'
 export default {
-  data() {
-    return {
-      user: {},
+  validations: {
+      firstName: {
+        required,
+        minLength: minLength(2)
+    },
+    lastName: {required},
+    emailId: {required},
+    password: {required},
+    confirmPassword: {required}
+  },
+
+  data: () => ({
+      firstName: '',
+      lastName: '',
+      emailId: '',
+      password: '',
+      confirmPassword: '',
+      user: {
+      },
       showPassword: false,
       isClicked: false,
       URL: "http://localhost:3000/registration",
-      /* fNameRules: [
-        name => !!name || "Enter first name",
-        name =>
-          /^[A-Za-z]{2,}$/.test(name) ||
-          "Name should contain minimum two characters"
-      ],
-      lNameRules: [name => !!name || "Enter last name"],
-      emailRules: [
-        email => !!email || "Enter email address",
-        email =>
-          /^([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*$/.test(
-            email
-          ) || "E-mail must be valid"
-      ],
-      passwordRules: [
-        password => !!password || "Enter password",
-        password =>
-          /^(?=.*[A-Z])(?=.*[0-9])(?=.*\W){1}.*$/.test(password) ||
-          "Use 8 or more characters with a mix of letters, numbers & symbols"
-      ] */
-      fNameRules: [],
+     /*  fNameRules: [],
       lNameRules: [],
       emailRules: [],
-      passwordRules: []
+      passwordRules: [] */
+  }),
 
-    };
+  computed:{
+      firstNameErrors () {
+      const errors = [];
+      if (!this.$v.firstName.$dirty) return errors;
+      !this.$v.firstName.required && errors.push('first name is required.');
+      return errors;
+    },
+      lastNameErrors () {
+      const errors = []
+      if (!this.$v.lastName.$dirty) return errors
+      !this.$v.lastName.required && errors.push('last name is required.')
+      return errors
+    },
+    emailIdErrors () {
+      const errors = []
+      if (!this.$v.emailId.$dirty) return errors
+      !this.$v.emailId.required && errors.push('email address is required.')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.required && errors.push('password is required.')
+      return errors
+    },
+    confirmPasswordErrors () {
+      const errors = []
+      if (!this.$v.confirmPassword.$dirty) return errors
+      !this.$v.confirmPassword.required && errors.push('confirm password is required.')
+      return errors
+    },
   },
   methods: {
-    validate() {alert("reached")
+    Register(){
+      this.$v.$touch()
+      this.isClicked = true
+    }
+    /* validate() {
       this.fNameRules= [
         name => !!name || "Enter first name",
         name =>
@@ -183,20 +216,8 @@ export default {
         
         alert("Password mismatch")
       }
-    },
-    registration() {
-      fetch(this.URL, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(this.post)
-      })
-        .then(res => res.json())
-        .then(data => console.log("data: " + data))
-        .catch(err => console.log(err));
-    }
+    }, */
+   
   }
 };
 </script>
---
+++ fundoo-notes-front-end/src/services/registration.svc.js
@@ -0,0 +1,13 @@
+/*  registration() {
+      fetch(this.URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.post)
+      })
+        .then(res => res.json())
+        .then(data => console.log("data: " + data))
+        .catch(err => console.log(err));
+    } */
