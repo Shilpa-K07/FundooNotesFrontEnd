c1ab2501f23a7aafc144e88a440058652d68956c
Shilpa-K07 <shilpa07udupi@gmail.com>
2021-01-09 18:31:38 +0530

    refactor: refactor registration.vue to have image path in a variable

--- fundoo-notes-front-end/src/main.js
+++ fundoo-notes-front-end/src/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
-/* import AxiosPlugin from 'vue-axios-cors' */
 import Vuetify from 'vuetify/lib/framework'
 import '@mdi/font/css/materialdesignicons.css'
 
 
 Vue.use(Vuetify)
 Vue.use(Vuelidate)
-/* Vue.use(AxiosPlugin) */
 /* Vue.config.productionTip = false */
 new Vue({
   vuetify : new Vuetify(),
