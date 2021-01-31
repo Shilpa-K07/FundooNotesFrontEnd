d343074bf06957cc73fad48bb82ae2c9c8c9006d
Shilpa-K07 <shilpa07udupi@gmail.com>
2021-01-31 19:31:03 +0530

    feat: write test cases for login component

--- fundoo-notes-front-end/tests/unit/login.spec.js
+++ fundoo-notes-front-end/tests/unit/login.spec.js
@@ -1,64 +1,77 @@
-//import { shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Login from '../../src/pages/Login.vue'
 import { createLocalVue, mount } from '@vue/test-utils';
-//import Vuetify from 'vuetify';
-import Vuelidate from 'vuelidate'
-import Vue from 'vue';
+import Vuetify from 'vuetify';
+import Vuelidate from 'vuelidate';
+import Title from '../../src/components/Title.vue';
+//import Vue from 'vue';
 
-const localVue = createLocalVue();
+/* const localVue = createLocalVue();
 localVue.use(Vuelidate)
 const factory = (vuetify) => mount(Login, {
   localVue,
   vuetify,
-});
-//import sinon from 'sinon'
-describe('Login.vue', () => {
+}); */
 
-  /* let vuetify;
+describe('Login.vue', () => {
+  let wrapper
 
   beforeEach(() => {
-    vuetify = new Vuetify();
-  }); */
-
-  it('check for error when emailId is passed',() => {
-
-    const wrapper = factory();
-    const emailId = 'shilpa07udupi@gmail.com';
-    wrapper.setData({ emailId: 'shilpa07udupi@gmail.com' })
-    //await Vue.nextTick();
-    expect(wrapper.vm.emailId).toBe(emailId);
-
-    
-  // const emailId = 'shilpa07udupi@gmail.com'
-/*     const wrapper = shallowMount(Login)
-    wrapper.setData({ emailId: 'shilpa07udupi@gmail.com' })
-    expect(wrapper.find('.error').exists()).toBeFalsy()
- */
-    //expect(wrapper.text()).toMatch(emailId)
-  })
-  it('check for error when password is passed',() => {
-    const wrapper = factory();
-    const password = 'Abcd@123';
-    wrapper.setData({ password: 'Abcd@123' })
-    expect(wrapper.vm.password).toBe(password);
-  // const emailId = 'shilpa07udupi@gmail.com'
-/*     const wrapper = shallowMount(Login)
-    wrapper.setData({ emailId: 'shilpa07udupi@gmail.com' })
-    expect(wrapper.find('.error').exists()).toBeFalsy()
- */
-    //expect(wrapper.text()).toMatch(emailId)
-  })
-
-
-  /* it('Click on login button calls our method "login"', async () => {
-    const spy = sinon.spy()
-    const wrapper = shallowMount(Login, {
-      propsData: {
-        callMe: spy
-      }
+    const localVue = createLocalVue()
+    localVue.use(Vuetify)
+    localVue.use(Vuelidate)
+    wrapper = shallowMount(Login, {
+      localVue,
     })
-    await wrapper.find('button.yes').trigger('click')
-  
-    spy.should.have.been.calledWith('yes')
-  }) */
+  })
+
+  it('emailId should evaluate to true when correct pattern is passed', () => {
+    wrapper.vm.emailId = 'shilpa07udupi@gmail.com'
+    expect(wrapper.vm.$v.emailId.email).toBe(true)
+  })
+
+  it('emailId should evaluate to false when "@" is missed', () => {
+    wrapper.vm.emailId = 'shilpa07udupigmail.com'
+    expect(wrapper.vm.$v.emailId.email).toBe(false)
+  })
+
+  it('emailId should evaluate to false when it is empty', () => {
+    wrapper.vm.emailId = ''
+    expect(wrapper.vm.$v.emailId.required).toBe(false)
+  })
+
+  it('password should evaluate to true when correct pattern is passed', () => {
+    wrapper.vm.password = 'Abcd@123'
+    expect(wrapper.vm.$v.password.isPasswordStrong).toBe(true)
+  })
+
+  it('password should evaluate to false when special character is missed', () => {
+    wrapper.vm.password = 'Abcd1234'
+    expect(wrapper.vm.$v.password.isPasswordStrong).toBe(false)
+  })
+
+  it('password should evaluate to false when digit is missed', () => {
+    wrapper.vm.password = 'Abcdse@we'
+    expect(wrapper.vm.$v.password.isPasswordStrong).toBe(false)
+  })
+
+  it('password should evaluate to false when uppercase is missed', () => {
+    wrapper.vm.password = 'abcd1234'
+    expect(wrapper.vm.$v.password.isPasswordStrong).toBe(false)
+  })
+
+  it('password should evaluate to false when it is empty', () => {
+    wrapper.vm.password = ''
+    expect(wrapper.vm.$v.password.required).toBe(false)
+  })
+
+  it('check if child Title exists', () => {
+    expect(wrapper.contains(Title)).toBe(true)
+  })
+
+  it('Check emitted "login" event', ()=>{
+    wrapper.vm.$emit('login')
+    expect(wrapper.emitted().login).toBeTruthy()
+  })
+
 })
